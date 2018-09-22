a64eefedc629e08730b501f21a174efb7d02f111
Eduardo Pereyra Yraola <eduardo.pereyrayraola@gmail.com>
2018-09-22 17:30:16 -0300

    Agregue las carpetas templates, libs y config. hay que arreglar los archivos #1 NinaEncinosa

---
+++ config/ConfigApp.php
@@ -0,0 +1,17 @@
+<?php
+
+  class ConfigApp
+  {
+    public static $ACTION = 'action';
+    public static $PARAMS = 'params';
+    public static $ACTIONS = [
+      '' => 'TareasControllerHome#Home',
+      'home' => 'TareasControllerHome#Home'
+    ];
+
+
+  }
+
+
+
+ ?>
--- route.php
+++ route.php
@@ -1,16 +1,37 @@
 <?php
-  require_once "inicio.php"
 
-  if($_GET['action'] == ''){
-    Inicio();
-  }else{
-    $partesURL = explode('/', $_GET['action']);
+require_once "config/ConfigApp.php";
+// require_once "controller/TareasController.php";
 
-    if($partesURL[0] === 'inicio'){
-      Inicio($partesURL[1],$partesURL[2]);
+function parseURL($url)
+{
+  $urlExploded = explode('/', $url);
+  $arrayReturn[ConfigApp::$ACTION] = $urlExploded[0];
+
+  #borrar/1/2/3/4
+  $arrayReturn[ConfigApp::$PARAMS] = isset($urlExploded[1]) ? array_slice($urlExploded,1) : null;
+  return $arrayReturn;
+}
+
+if(isset($_GET['action'])){
+   #$urlData[ACTION] = borrar
+   #$urlData[PARAMS] = [1,2,3,4]
+    $urlData = parseURL($_GET['action']);
+    $action = $urlData[ConfigApp::$ACTION]; //home
+    if(array_key_exists($action,ConfigApp::$ACTIONS)){
+        $params = $urlData[ConfigApp::$PARAMS];
+        $action = explode('#',ConfigApp::$ACTIONS[$action]);
+        $controller =  new $action[0]();
+        $metodo = $action[1];
+        if(isset($params) &&  $params != null){
+            echo $controller->$metodo($params);
+        }
+        else{
+            echo $controller->$metodo();
+        }
+    }else{
+      // $controller =  new TareasController();
+      // echo $controller->Home();
     }
-
-  }
-
-
+}
  ?>
