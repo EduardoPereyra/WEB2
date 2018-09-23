f52f7df7c006d3dbd534626344d0167278c55768
Eduardo Pereyra Yraola <eduardo.pereyrayraola@gmail.com>
2018-09-23 18:11:31 -0300

    acomode el repositorio

--- config/ConfigApp.php
+++ config/ConfigApp.php
@@ -6,11 +6,7 @@
     public static $PARAMS = 'params';
     public static $ACTIONS = [
       '' => 'Accionescontroller#Home',
-      'home' => 'AccionesController#Home',
-      'memes' => 'AccionesController#Memes',
-      'personajes' => 'AccionesController#Personajes',
-      'shopping' => 'AccionesController#Shopping',
-      'info' => 'AccionesController#Info'
+      'home' => 'AccionesController#Home'
     ];
 
 
