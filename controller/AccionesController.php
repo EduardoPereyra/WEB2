cd39733d63ce46e5247b665f3130c6ca86b38fb7
Eduardo Pereyra Yraola <eduardo.pereyrayraola@gmail.com>
2018-09-24 17:27:09 -0300

    modifique accionescontroller.php en rama2

--- controller/AccionesController.php
+++ controller/AccionesController.php
@@ -18,32 +18,4 @@ class AccionesController
       $Tareas = $this->model->GetTareas();
       $this->view->Mostrar($this->Titulo, $Tareas);
   }
-
-  function InsertTarea(){
-    $titulo = $_POST["tituloForm"];
-    $descripcion = $_POST["descripcionForm"];
-
-    if(isset($_POST["completadaForm"])){
-      $completada = 1;
-    }else{
-      $completada = 0;
-    }
-
-    $this->model->InsertarTarea($titulo,$descripcion,$completada);
-
-    header("Location: http://".$_SERVER["SERVER_NAME"] . dirname($_SERVER["PHP_SELF"]));
-  }
-
-  function BorrarTarea($param){
-    $this->model->BorrarTarea($param[0]);
-    header("Location: http://".$_SERVER["SERVER_NAME"] . dirname($_SERVER["PHP_SELF"]));
-  }
-
-  function CompletarTarea($param){
-    $this->model->CompletarTarea($param[0]);
-    header("Location: http://".$_SERVER["SERVER_NAME"] . dirname($_SERVER["PHP_SELF"]));
-
-  }
-}
-
 ?>
