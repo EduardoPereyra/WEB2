30d99c696e254b78e578ebb7420842c56c3661bf
Eduardo Pereyra Yraola <eduardo.pereyrayraola@gmail.com>
2018-09-24 16:24:04 -0300

    Configure ConfigApp.php en Rama1

--- config/ConfigApp.php
+++ config/ConfigApp.php
@@ -6,12 +6,11 @@
     public static $PARAMS = 'params';
     public static $ACTIONS = [
       '' => 'Accionescontroller#Home',
-      'home' => 'AccionesController#Home'
+      'home' => 'AccionesController#Home',
+      'info' => 'AccionesController#Info',
+      'memes' => 'AccionesController#Memes',
+      'personajes' => 'AccionesController#Personajes',
+      'shopping' => 'AccionesController#Shopping'
     ];
-
-
   }
-
-
-
- ?>
+?>
